99e1e71c767ff8e610ba48779320095fc3ca86bb
sdupourque <sdupourque@irap.omp.eu>
2022-11-20 11:59:53 +0100

    updated config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'jaxspec'
+copyright = '2022, Dupourqué'
+author = 'Simon Dupourqué'
 
 release = '0.1'
 version = '0.1.0'
