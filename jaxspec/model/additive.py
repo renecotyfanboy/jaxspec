59439a4a613f9dc29efe06f5902da77edf419dcb
sdupourque <sdupourque@irap.omp.eu>
2022-11-25 00:15:02 +0100

    added Lorentz model

--- jaxspec/model/additive.py
+++ jaxspec/model/additive.py
@@ -12,3 +12,14 @@ class PowerLaw(AdditiveComponent):
         norm = hk.get_parameter('norm', [], init=Constant(1))
 
         return norm*energy**(-alpha)
+
+
+class Lorentz(AdditiveComponent):
+
+    def __call__(self, energy):
+
+        line_energy = hk.get_parameter('E_l', [], init=Constant(1))
+        sigma = hk.get_parameter('sigma', [], init=Constant(1))
+        norm = hk.get_parameter('norm', [], init=Constant(1))
+
+        return norm*(sigma/(2*jnp.pi))/((energy-line_energy)**2 + (sigma/2)**2)
