e8ada1fdcf49259c6c02e66e99f367e40c60f4cc
sdupourque <sdupourque@irap.omp.eu>
2022-11-25 11:20:34 +0100

    cleaning some clunky parts thanks to lazy evaluation of models

--- jaxspec/model/__init__.py
+++ jaxspec/model/__init__.py
@@ -6,8 +6,7 @@ from .multiplicative import *
 
 
 # I want to put all the existent models in the namespace to build them in the good ol' XSPEC way
-_modules = {name.lower(): cls for name, cls in inspect.getmembers(sys.modules[__name__], inspect.isclass)
-            if issubclass(cls, ModelComponent)}
-
-# Find this pretty ugly, but didn't fix it yet
-del _modules['modelcomponent'], _modules['additivecomponent'], _modules['multiplicativecomponent']
+# No cherry picking for ModelComponents is required since they are lazily called
+# and not built until explicitly called
+# Maybe add some safeguard at some point
+_modules = {name.lower(): cls for name, cls in inspect.getmembers(sys.modules[__name__], inspect.isclass)}
