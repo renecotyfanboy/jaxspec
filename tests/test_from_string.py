22636b0b457706c83e47a5c2e2d5aa2167fac77f
sdupourque <sdupourque@irap.omp.eu>
2022-11-28 23:14:50 +0100

    additional tests

---
+++ tests/test_from_string.py
@@ -0,0 +1,48 @@
+import chex
+import jax.numpy as jnp
+from jax import vmap, grad
+from jaxspec.model.from_string import build_model
+
+chex.set_n_cpu_devices(n=2)
+
+
+class TestFromString(chex.TestCase):
+
+    def setUp(self):
+        from jaxspec.model import _modules
+        self.module_dict = _modules.items()
+        self.model_string = 'expfac*lorentz'
+        self.energy = jnp.geomspace(0.1, 100, 50)
+
+    @chex.all_variants
+    def test_build_model_str(self):
+        """
+        Test building a model from fixed string and evaluating it
+        """
+
+        model = build_model(model_string=self.model_string)
+
+        @self.variant
+        def f(inputs): return model.apply(model.init(None, self.energy), inputs)
+
+        out = f(self.energy)
+
+        self.assertEqual(out.shape, self.energy.shape, f"'{self.model_string}' changes input shape")
+
+    @chex.all_variants
+    def test_grad_model_str(self):
+        """
+        Test building a model from fixed string and evaluating its gradient
+        """
+
+        model = build_model(model_string=self.model_string)
+
+        @self.variant
+        def f(inputs):
+            init = model.init(None, self.energy)
+            func = lambda e : model.apply(init, e)
+            return grad(func)(inputs)
+
+        grad_value = vmap(f)(self.energy)
+
+        self.assertEqual(jnp.sum(jnp.isnan(grad_value)), 0, f'{self.model_string} gradient returns NaNs')
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,16 @@
 import chex
+from jax import grad, vmap
 import jax.numpy as jnp
 import haiku as hk
-from jaxspec.model import _modules
 
+chex.set_n_cpu_devices(n=2)
 
 class TestModules(chex.TestCase):
+
+    def setUp(self):
+        from jaxspec.model import _modules
+        self.module_dict = _modules.items()
+        self.energy = jnp.geomspace(0.1, 100, 50)
 
     @chex.all_variants
     def test_all_modules(self):
@@ -12,17 +18,23 @@ class TestModules(chex.TestCase):
         Test to evaluate energies with every model components
         """
 
-        energy = jnp.geomspace(0.1, 100, 50)
-
-        for name, module in _modules.items():
-
+        for name, module in self.module_dict:
             @hk.testing.transform_and_run(jax_transform=self.variant)
-            def f(inputs):
-                return module()(inputs)
+            def f(inputs): return module()(inputs)
+            out = f(self.energy)
 
-            try:
-                out = f(energy)
-            except Exception:
-                self.fail(f'{name} component failed')
+            self.assertEqual(out.shape, self.energy.shape, f'{name} changes input shape')
 
-            self.assertEqual(out.shape, energy.shape)
+    @chex.all_variants
+    def test_all_modules_grad(self):
+        """
+        Test to evaluate gradient of every model component
+        """
+
+        for name, module in self.module_dict:
+            @hk.testing.transform_and_run(jax_transform=self.variant)
+            def f(inputs): return hk.value_and_grad(module())(inputs)
+
+            out, grad = vmap(f)(self.energy)
+
+            self.assertEqual(jnp.sum(jnp.isnan(grad)), 0, f'{name} gradient returns NaNs')
