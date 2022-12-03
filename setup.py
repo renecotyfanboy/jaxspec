b80bdaf0f4cf5827c3da43a73417f7f03d30597a
sdupourque <sdupourque@irap.omp.eu>
2022-12-03 15:56:36 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='jaxspec',    # This is the name of your PyPI-package.
+    version='0.1.0',
+    description='Python package for fitting X-Ray spectra with JAX',
+    author='Simon Dupourqué',
+    author_email='sdupourque@irap.omp.eu',
+    url="https://github.com/renecotyfanboy/jaxspec",
+    packages=['jaxspec']
+)
