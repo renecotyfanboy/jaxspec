1f05557487decac0fe10fe59314eb65ee34c78d3
sdupourque <sdupourque@irap.omp.eu>
2022-12-03 17:04:51 +0100

    trying to use pbr

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup
 
-
 setup(
-    name='jaxspec',    # This is the name of your PyPI-package.
-    version='0.1.0',
-    description='Python package for fitting X-Ray spectra with JAX',
-    author='Simon Dupourqué',
-    author_email='sdupourque@irap.omp.eu',
-    url="https://github.com/renecotyfanboy/jaxspec",
-    packages=['jaxspec']
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
