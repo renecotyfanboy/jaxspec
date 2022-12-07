bc5e302025eee5498eece540b50715f7f0638971
sdupourque <sdupourque@irap.omp.eu>
2022-12-07 18:43:16 +0100

    implement TbAbs!

--- jaxspec/model/multiplicative.py
+++ jaxspec/model/multiplicative.py
@@ -2,6 +2,7 @@ import haiku as hk
 import jax.numpy as jnp
 from .abc import MultiplicativeComponent
 from haiku.initializers import Constant
+from astropy.table import Table
 
 
 class Expfac(MultiplicativeComponent):
@@ -17,3 +18,29 @@ class Expfac(MultiplicativeComponent):
         pivot = hk.get_parameter('E_c', [], init=Constant(1))
 
         return jnp.where(energy >= pivot, 1. + amplitude*jnp.exp(-factor*energy), 1.)
+
+
+class TbAbs(MultiplicativeComponent):
+    r"""
+    The Tuebingen-Boulder ISM absorption model
+
+    Parameters
+    ----------
+        :math:`N_H` : equivalent hydrogen column density :math:`\left[\frac{\text{atoms}~10^{22}}{\text{cm}^2}\right]`
+
+    """
+    def __init__(self):
+
+        super(TbAbs, self).__init__()
+        # Fixing incoming path issues
+        path = 'jaxspec/tables/xsect_tbabs_wilm.fits'
+        table = Table.read(path)
+        self.energy = jnp.asarray(table['ENERGY'])
+        self.sigma = jnp.asarray(table['SIGMA'])
+
+    def __call__(self, energy):
+
+        nh = hk.get_parameter('N_H', [], init=Constant(1))
+        sigma = jnp.interp(energy, self.energy, self.sigma, left=jnp.inf, right=0.)
+
+        return jnp.exp(-nh*sigma)
