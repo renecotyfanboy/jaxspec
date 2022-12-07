2938c0fa637e666dc93ac48c180245b14872c563
sdupourque <sdupourque@irap.omp.eu>
2022-12-07 18:03:15 +0100

    remove buggy features

--- jaxspec/model/additive.py
+++ jaxspec/model/additive.py
@@ -44,8 +44,8 @@ class Lorentz(AdditiveComponent):
 
     def __call__(self, energy):
 
-        line_energy = hk.get_parameter('E_l', [], init=self.E_l_init)
-        sigma = hk.get_parameter('sigma', [], init=self.sigma_init)
-        norm = hk.get_parameter('norm', [], init=self.norm_init)
+        line_energy = hk.get_parameter('E_l', [], init=Constant(1))
+        sigma = hk.get_parameter('sigma', [], init=Constant(1))
+        norm = hk.get_parameter('norm', [], init=Constant(1))
 
         return norm*(sigma/(2*jnp.pi))/((energy-line_energy)**2 + (sigma/2)**2)
