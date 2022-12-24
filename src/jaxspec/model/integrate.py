6ba692b181bb95cb917cbdfff429ea10720164ad
sdupourque <sdupourque@irap.omp.eu>
2022-12-24 11:30:41 +0100

    DEQUAD + TRAPZ

--- src/jaxspec/model/integrate.py
+++ src/jaxspec/model/integrate.py
@@ -2,15 +2,7 @@ import haiku as hk
 import jax.numpy as jnp
 
 
-def phi(t):
-    return jnp.tanh(jnp.pi / 2 * jnp.sinh(t))
-
-
-def dphi(t):
-    return jnp.pi / 2 * jnp.cosh(t) * (1/jnp.cosh(jnp.pi / 2 * jnp.sinh(t))**2)
-
-
-class Integrate(hk.Module):
+class IntegrateDEQUAD(hk.Module):
     r"""
     Module for integrating a model between two energies. It relies on double exponential quadrature [1]_ [2]_ [3]_ for
     finite intervals to compute an approximation of the integral of a model.
@@ -23,14 +15,21 @@ class Integrate(hk.Module):
 
 
     """
+    @staticmethod
+    def phi(t):
+        return jnp.tanh(jnp.pi / 2 * jnp.sinh(t))
+
+    @staticmethod
+    def dphi(t):
+        return jnp.pi / 2 * jnp.cosh(t) * (1 / jnp.cosh(jnp.pi / 2 * jnp.sinh(t)) ** 2)
 
     def __init__(self, model: hk.Module, n_points=71):
-        super(Integrate, self).__init__()
+        super(IntegrateDEQUAD, self).__init__()
         self.model = model
         self.n = n_points
         self.t = jnp.linspace(-4, 4, self.n)
-        self.x = phi(self.t)
-        self.dx = dphi(self.t)
+        self.x = self.phi(self.t)
+        self.dx = self.dphi(self.t)
 
 
     def __call__(self, E_min, E_max):
@@ -40,3 +39,20 @@ class Integrate(hk.Module):
         dx = (E_max-E_min)/2 * self.dx
 
         return jnp.trapz(self.model(x) * dx, x=self.t)
+
+
+class IntegrateTRAPZ(hk.Module):
+    r"""
+    Module for integrating a model between two energies. It relies on the well-known trapezoidal rule.
+    """
+
+    def __init__(self, model: hk.Module, n_points=71):
+        super(IntegrateTRAPZ, self).__init__()
+        self.model = model
+        self.n = n_points
+
+    def __call__(self, E_min, E_max):
+
+        #Integrate in log space
+        x = jnp.linspace(E_min, E_max, self.n)
+        return jnp.trapz(self.model(x)*x, x=jnp.log(x))
