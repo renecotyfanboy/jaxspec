7545b56eb6c0e6cdd15daf9ddb5788a6a26156c9
sdupourque <sdupourque@irap.omp.eu>
2023-04-20 16:50:19 +0200

    correction for M1 and M2

--- src/jaxspec/data/util.py
+++ src/jaxspec/data/util.py
@@ -3,6 +3,6 @@ from .observation import Observation
 
 example_observations = {
     'PN': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/PN.pha', low_energy=0.3, high_energy=12),
-    'MOS1': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS1.pha', low_energy=0.3, high_energy=12),
-    'MOS2': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS2.pha', low_energy=0.3, high_energy=12)
+    'MOS1': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS1.pha', low_energy=0.3, high_energy=7),
+    'MOS2': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS2.pha', low_energy=0.3, high_energy=7)
 }
