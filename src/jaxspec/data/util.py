203ea76eb4ccaa617b0ad5a8c86b68aed21f868a
sdupourque <sdupourque@irap.omp.eu>
2023-04-20 16:38:31 +0200

    better energy cut for demo observations

--- src/jaxspec/data/util.py
+++ src/jaxspec/data/util.py
@@ -2,7 +2,7 @@ import importlib.resources
 from .observation import Observation
 
 example_observations = {
-    'PN': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/PN.pha'),
-    'MOS1': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS1.pha'),
-    'MOS2': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS2.pha')
+    'PN': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/PN.pha', low_energy=0.3, high_energy=12),
+    'MOS1': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS1.pha', low_energy=0.3, high_energy=12),
+    'MOS2': Observation.from_pha_file(importlib.resources.files('jaxspec') / 'data/example_data/MOS2.pha', low_energy=0.3, high_energy=12)
 }
