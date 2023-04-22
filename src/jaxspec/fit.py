5f8532f3ae4162a0fc35842d53597d818ee73944
sdupourque <sdupourque@irap.omp.eu>
2023-04-23 00:13:56 +0200

    tryouts for jitting model

--- src/jaxspec/fit.py
+++ src/jaxspec/fit.py
@@ -2,6 +2,7 @@ import haiku as hk
 import jax.numpy as jnp
 import arviz as az
 import numpyro
+import jax
 import numpyro.distributions as dist
 from abc import ABC, abstractmethod
 from jax import random
@@ -14,6 +15,38 @@ from numpyro.infer.mcmc import MCMCKernel
 from numpyro.distributions import Distribution, Poisson
 from typing import Union
 
+
+def build_prior(prior):
+    """
+    Build the prior distribution for the model parameters.
+    """
+
+    parameters = hk.data_structures.to_haiku_dict(prior)
+
+    for i, (m, n, to_set) in enumerate(hk.data_structures.traverse(prior)):
+
+        if isinstance(to_set, Distribution):
+            parameters[m][n] = numpyro.sample(f'{m}_{n}', to_set)
+
+    return parameters
+
+
+class ForwardModel(hk.Module):
+
+    def __init__(self, model: SpectralModel, observation: Observation):
+        super().__init__()
+        self.model = model
+        self.observation = observation
+
+    def __call__(self, parameters):
+        """
+        Compute the count functions for a given observation.
+        """
+
+        energies = jnp.asarray(self.observation.energies, dtype=jnp.float64)
+        transfer_matrix = jnp.asarray(self.observation.transfer_matrix, dtype=jnp.float64)
+
+        return jnp.clip(transfer_matrix @ jnp.trapz(self.model(parameters, energies), x=energies, axis=0), a_min=1e-6)
 
 class ForwardModelFit(ABC):
     """
@@ -30,33 +63,6 @@ class ForwardModelFit(ABC):
         self.model = model
         self.observation = [observation] if isinstance(observation, Observation) else observation
         self.pars = tree_map(lambda x: jnp.float64(x), self.model.params)
-
-    def model_observation(self, parameters, observation: Observation):
-        """
-        Compute the count functions for a given observation.
-        """
-
-        energies = jnp.asarray(observation.energies, dtype=jnp.float64)
-        transfer_matrix = jnp.asarray(observation.transfer_matrix, dtype=jnp.float64)
-
-        return jnp.clip(transfer_matrix @ jnp.trapz(self.model(parameters, energies), x=energies, axis=0), a_min=1e-6)
-
-    def log_likelihood_observation(self, parameters, observation):
-        """
-        Compute the log likelihood for a given observation.
-        """
-
-        model = self.model_observation(parameters, observation)
-        observed = observation.observed_counts
-
-        return -Poisson(model).log_prob(observed).sum()
-
-    def log_likelihood(self, parameters):
-        """
-        Compute the joint likelihood for all the observations.
-        """
-
-        return sum(tree_map(lambda x: self.log_likelihood_observation(parameters, x), self.observation))
 
     @abstractmethod
     def fit(self, *args, **kwargs):
@@ -86,20 +92,6 @@ class BayesianModel(ForwardModelFit):
     def __init__(self, model, observation):
         super().__init__(model, observation)
 
-    def build_prior(self, prior):
-        """
-        Build the prior distribution for the model parameters.
-        """
-
-        parameters = hk.data_structures.to_haiku_dict(prior)
-
-        for i, (m, n, to_set) in enumerate(hk.data_structures.traverse(prior)):
-
-            if isinstance(to_set, Distribution):
-                parameters[m][n] = numpyro.sample(f'{m}_{n}', to_set)
-
-        return parameters
-
     def fit(self,
             prior_params,
             rng_key: int = 0,
@@ -108,18 +100,27 @@ class BayesianModel(ForwardModelFit):
             num_samples: int = 1000,
             likelihood: Distribution = Poisson,
             kernel: MCMCKernel = NUTS,
+            jit_model: bool = False,
             kernel_kwargs: dict = {},
             mcmc_kwargs: dict = {},
             return_inference_data: bool = True):
 
-        def model():
+        def bayesian_model():
 
-            parameters = self.build_prior(prior_params)
+            pars = build_prior(prior_params)
 
             for i, obs in enumerate(self.observation):
 
+                transformed_model = hk.without_apply_rng(hk.transform(lambda pars: ForwardModel(self.model, obs)(pars)))
+
+                if jit_model:
+                    obs_model = jax.jit(lambda p: transformed_model.apply(None, p))
+
+                else:
+                    def obs_model(p): transformed_model.apply(None, p)
+
                 numpyro.sample(f'likelihood_obs_{i}',
-                               likelihood(self.model_observation(parameters, obs)),
+                               likelihood(obs_model(pars)),
                                obs=obs.observed_counts)
 
         chain_kwargs = {
@@ -128,7 +129,7 @@ class BayesianModel(ForwardModelFit):
             'num_chains': num_chains
         }
 
-        kernel = kernel(model, **kernel_kwargs)
+        kernel = kernel(bayesian_model, **kernel_kwargs)
         mcmc = MCMC(kernel, **(chain_kwargs | mcmc_kwargs))
 
         mcmc.run(random.PRNGKey(rng_key))
