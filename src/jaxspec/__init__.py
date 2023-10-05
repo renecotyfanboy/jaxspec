1570bdbb9caa5a9c45bad42842de0be48a8b671e
sdupourque <sdupourque@irap.omp.eu>
2023-10-05 23:52:06 +0200

    tryouts using jupyter notebooks

--- src/jaxspec/__init__.py
+++ src/jaxspec/__init__.py
@@ -1,13 +1,1 @@
-"""
-I am trying to build some doc automatically here, tell me if this works ! =D
-
-.. currentmodule:: jaxspec
-
-Model
-^^^^^
-
-.. automodule:: model
-    :members:
-"""
-
 __version__ = "0.1.0"
