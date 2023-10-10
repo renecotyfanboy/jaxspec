408e395a96ac8dd21e442bb2ddef21d85afec80d
sdupourque <sdupourque@irap.omp.eu>
2023-10-10 17:12:19 +0200

    drafty fakeit setup, to be improved later

--- docs/references/data.md
+++ docs/references/data.md
@@ -1,16 +1,35 @@
-# Data containers
+## Generating mock data
+
+::: jaxspec.data.util.fakeit
+    options:
+      show_root_heading: true
+      show_root_full_path: true
+      show_root_toc_entry: true
+      heading_level: 3
+
+::: jaxspec.data.util.fakeits
+    options:
+      show_root_heading: true
+      show_root_full_path: true
+      show_root_toc_entry: true
+      heading_level: 3
+
+## Data containers
 
 ::: jaxspec.data.instrument
     options:
       show_root_heading: false
       show_root_toc_entry: false
+      heading_level: 3
 
 ::: jaxspec.data.observation
     options:
       show_root_heading: false
       show_root_toc_entry: false
+      heading_level: 3
 
 ::: jaxspec.data.ogip
     options:
       show_root_heading: false
-      show_root_toc_entry: false
+      show_root_toc_entry: false 
+      heading_level: 3
--- src/jaxspec/analysis/results.py
+++ src/jaxspec/analysis/results.py
@@ -1,6 +1,11 @@
 import arviz as az
+from collections.abc import Mapping
+from typing import TypeVar
 from numpyro.infer import MCMC
+from haiku.data_structures import traverse
 
+K = TypeVar("K")
+V = TypeVar("V")
 
 class ResultContainer:
 
@@ -8,11 +13,21 @@ class ResultContainer:
 
 
 class ChainResult(ResultContainer):
-
-    def __init__(self, mcmc: MCMC):
+    #TODO : Add docstring
+    #TODO : Add type hints
+    #TODO : Add proper separation between params and samples, cf from haiku and numpyro
+    def __init__(self,
+                 mcmc: MCMC,
+                 structure: Mapping[K, V]):
 
         self.mcmc = mcmc
         self.samples = mcmc.get_samples()
+        self.params = {}
+
+        for module, parameter, value in traverse(structure):
+            if self.params.get(module, None) is None:
+                self.params[module] = {}
+            self.params[module][parameter] = self.samples[f'{module}_{parameter}']
 
     @property
     def inference_data(self):
