0c05fae264b406b1b8f774e607ffcf76137751b0
sdupourque <sdupourque@irap.omp.eu>
2023-10-25 14:21:32 +0200

    enforcing xml coverage export

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import subprocess
 from unittest import TestCase
+import runpy
 
 # Allow relative imports for github workflows
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,34 +11,10 @@ sys.path.append(source_dir)
 
 class TestDocs(TestCase):
     def test_basic_fit(self):
-        result = subprocess.run(
-            ["python3", os.path.join(source_dir, "docs/examples/plot_1_basic_fit.py")],
-            capture_output=True,
-            text=True,
-        )
-
-        self.assertEqual(result.returncode, 0, f"Erreur : {result.stderr}")
+        runpy.run_module("docs.examples.plot_1_basic_fit", run_name="__main__")
 
     def test_build_model(self):
-        result = subprocess.run(
-            [
-                "python3",
-                os.path.join(source_dir, "docs/examples/plot_2_build_model.py"),
-            ],
-            capture_output=True,
-            text=True,
-        )
-
-        self.assertEqual(result.returncode, 0, f"Erreur : {result.stderr}")
+        runpy.run_module("docs.examples.plot_2_build_model", run_name="__main__")
 
     def test_custom_component(self):
-        result = subprocess.run(
-            [
-                "python3",
-                os.path.join(source_dir, "docs/examples/plot_3_custom_component.py"),
-            ],
-            capture_output=True,
-            text=True,
-        )
-
-        self.assertEqual(result.returncode, 0, f"Erreur : {result.stderr}")
+        runpy.run_module("docs.examples.plot_3_custom_component", run_name="__main__")
