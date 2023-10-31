9f917cc4473bdc98f4463b25daa2fc79c4a42b26
sdupourque <sdupourque@irap.omp.eu>
2023-10-31 14:23:35 +0100

    new fakeit tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -4,4 +4,6 @@ from mkdocs_gallery.gen_gallery import DefaultResetArgv
 warnings.filterwarnings("ignore")
 
 conf = {
+    "ignore_pattern": "doc_variable.py",
+    "reset_argv": DefaultResetArgv(),
 }
