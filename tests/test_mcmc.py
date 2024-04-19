39b9838c853ca1ebfb13c942e65980714c9bb5ac
sdupourque <sdupourque@irap.omp.eu>
2024-04-19 14:05:37 +0200

    correcting test convergence

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -1,3 +1,3 @@
-def test_convergence(get_individual_mcmc_results, get_joint_result):
-    for result in get_individual_mcmc_results + get_joint_result:
+def test_convergence(get_individual_mcmc_results, get_joint_mcmc_result):
+    for result in get_individual_mcmc_results + get_joint_mcmc_result:
         assert result.converged
